748450783cb3009b3e08ac8d10b53d9eef2a7e96
Juan Osorio Barrera <ext_josoriob@falabella.cl>
2022-09-17 18:05:15 -0300

    added currency codes

--- structs/structs.go
+++ structs/structs.go
@@ -1,5 +1,9 @@
 package structs
 
+import (
+	constants "github.com/osoriojuan/go-integration-grcp/constants"
+)
+
 type DefaultRequest struct {
 	Operation string `json:"operation"`
 	Message   string `json:"message"`
@@ -10,4 +14,9 @@ type DefaultResponse struct {
 	Result  string `json:"operation"`
 	Message string `json:"message"`
 	Code    int    `json:"code"`
+}
+
+type ExchangeRateRequest struct {
+	ID                   string `json:"id"`
+	CurrenciesComparison constants.CurrencyType
 }
